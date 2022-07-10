244e1d92b9557d05bfedde06febc1539eb29618b
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-07-10 02:10:40 +0000

    update commit8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "public/homes#top"
   namespace :admin do
     resources :genres
   end
@@ -21,6 +22,6 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-root to: "homes#top"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
