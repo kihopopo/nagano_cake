3c1224567efd0af6cdeaf7c888606cf33210b307
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-07-13 01:49:12 +0000

    update commmit14

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -15,11 +15,20 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])  
   end
 
   def edit
+    @item = Item.find(params[:id])  
+    @genres = Genre.all
   end
   
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_items_path(@item.id)
+  end
+
   private
   
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
     belongs_to :genre
     enum is_active: {"販売中": true,"販売準備中": false}
+    def add_tax_price
+        (self.price*1.10).round
+    end
 end
