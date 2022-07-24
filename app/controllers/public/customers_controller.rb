123016f73983cad17712dda3727d57fce8e3076d
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-07-24 05:12:00 +0000

    update commit24

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,7 +3,7 @@ class Public::CustomersController < ApplicationController
   end
   
   def show
-    @customer = Customer.find(params[:id])  
+    @customer = current_customer
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  root to: "public/homes#top"
-  get '/about' => 'public/homes#about'
-  
-  get 'customers/my_page' => 'public/customers#show'
-  
-  namespace :admin do
-    resources :genres
-  end
-  namespace :admin do
-    resources :items
-  end
-  namespace :admin do
-    resources :customers
-  end
-  
-  namespace :public do
-    get 'items/index'
-  end
   devise_for :admin,skip: [:registrations, :passwords],controllers: {
   sessions: "admin/sessions"
 }
@@ -30,6 +7,23 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+  
+  root to: "public/homes#top"
+  get '/about' => 'public/homes#about'
+  
+  get 'customers/my_page' => 'public/customers#show'
+  
+  namespace :admin do
+    resources :genres
+    resources :items
+    resources :customers
+  end
+  
+  
+  namespace :public do
+    get 'items/index'
+  end
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
