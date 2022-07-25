8f0a9790aaffe0fee544c009bd248e57ae912f84
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-07-25 15:53:04 +0000

    update commit29

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])  
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :genre_id, :price, :is_active, :image)
   end
 end
