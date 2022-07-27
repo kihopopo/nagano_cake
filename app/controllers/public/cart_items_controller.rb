814a85b37031b267e853aab189df0c7f6624a594
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-07-27 06:10:15 +0000

    update commit31

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,19 @@
 class Public::CartItemsController < ApplicationController
   def index
+    #@cart_items = Cart_item.all
+  end
+  
+  
+
+
+
+
+
+
+
+
+   private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
