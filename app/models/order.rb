92a324f8a729cddee9706f50b1a1ee0f15bdbf16
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-08-05 13:19:00 +0000

    update commit 43

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :customer
 
+def shipping_cost
+  800
+end
+
+def total_payment
+  shipping_cost ＋ @total
+end
+
 
   enum payment_method: { credit_card: 0, transfer: 1 }
 end
